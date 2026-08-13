b5a1d58c8fa6ff1e01898945be00dbb545b23166
KAESER <yeferson.utima@kaeser.com>
2026-08-13 14:58:40 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,5 @@
-const CACHE_NAME = 'kaeser-filtros-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;900&display=swap',
-  'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
-];
-
-// Instalación: Guardar recursos en la memoria caché para uso Offline
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-// Activación
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Interceptar peticiones para servir datos desde la caché sin internet
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(fetch(event.request));
 });
